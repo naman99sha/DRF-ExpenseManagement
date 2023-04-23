cff24f3e6e26453aed0d02149059d89dfbe57258
Naman Sharma <naman99sha@gmail.com>
2023-04-23 11:55:25 +0530

    Created API for adding money to the bank account data

--- ExpenseManagementProject/ExpenseManagementProject/urls.py
+++ ExpenseManagementProject/ExpenseManagementProject/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('Users.urls',namespace="users"))
+    path('user/', include('Users.urls',namespace="users")),
+    path('expense/',include('ExpenseSheet.urls')),
 ]
--- ExpenseManagementProject/ExpenseSheet/views.py
+++ ExpenseManagementProject/ExpenseSheet/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .serializers import ExpenseEntry, ExpenseUserSerializer
+from .models import *
+from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.views import APIView
+from rest_framework import status
+import jwt
+from django.conf import settings
+from Users.models import User
 
-# Create your views here.
+class addBankBalance(APIView):
+    def post(self, request):
+        token = request.COOKIES.get('jwt')
+        if not token:
+            raise AuthenticationFailed("Unauthenticated, Please Login first")
+        
+        try:
+            payload = jwt.decode(
+                token, settings.API_SECRET, algorithms = ['HS256']
+            )
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed("Unauthenticated, Please Login Again")
+        
+        userObj = get_object_or_404(User, id=payload["id"]) or None
+        if not userObj:
+            return Response({"message":"User not found"}, status=status.HTTP_404_NOT_FOUND)
+        bankUserObj = get_object_or_404(ExpenseUserData, userObj=userObj) or None
+        if bankUserObj:
+            try:
+                amount = float(request.data["amount"])
+            except:
+                return Response({"message":"Either amount is not passed, or an invalid amount passed. Amount should be numeric"}, status=status.HTTP_400_BAD_REQUEST)
+            bankUserObj.bankBalance += amount
+            bankUserObj.save()
+            bankUserSerializer = ExpenseUserSerializer(bankUserObj)
+            return Response(bankUserSerializer.data, status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response({"message":"Bank details for this user does not exist"},status=status.HTTP_404_NOT_FOUND)
+            
